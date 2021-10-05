202acbcc441ac6c62affdf499b63ab0a64975491
Marcelo Gonçalves <marcelo_goncalves@id.uff.br>
2021-10-05 13:55:23 -0300

    feat: changed bot subject to be a trivia bot

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,47 @@
-import { Discord, Command, CommandMessage, CommandNotFound, On, ArgsOf } from "@typeit/discord";
-import { channel } from "diagnostics_channel";
+import {
+  Discord,
+  Command,
+  CommandMessage,
+  CommandNotFound,
+  On,
+  ArgsOf,
+} from "@typeit/discord";
 
-@Discord()
+@Discord("!")
 export class AppDiscord {
-  @On("message")
-  private onMessage([message]: ArgsOf<"message">){
-    return message.channel.id === "893974354105364500" ? console.log(message.content) : "";
-  }
-
   @Command("hello")
   private hello(message: CommandMessage) {
     message.reply("hello!");
   }
 
+  @Command("trivia")
+  private trivia(message: CommandMessage) {
+    const filter = (m) => m.author.id === message.author.id;
+    message.reply(
+      "Qual é o meu nome?```1 - Marcelo```\n```2 - Matheus```"
+    );
+    message.channel
+      .awaitMessages(filter, {
+        max: 1,
+        time: 10000,
+      })
+      .then(async (collected) => {
+        if (collected.first().content == "2") {
+          message.reply("sua resposta está errada!");
+        }
+        message.reply("resposta correta!");
+      });
+  }
+
   @Command("commandlist")
   private commandlist(message: CommandMessage) {
     message.reply(
-      "Os comandos disponíveis são:```!hello: devolve uma mensagem de boas-vindas em resposta ao usuário\n\n!teste: isso é um teste```"
+      "Os comandos disponíveis são:```!hello: devolve uma mensagem de boas-vindas em resposta ao usuário\n\n!trivia: inicia um jogo de quiz com o usuário```"
     );
   }
 
-
   @CommandNotFound()
   private notFound(message: CommandMessage) {
-    message.reply("this command does not exist.");
+    return message.reply("this command does not exist.");
   }
 }
-
-
